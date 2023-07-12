888862a2dbd2cd5447647316365ce6371e8462f5
Jose Sánchez <sanchez069@gmail.com>
2023-07-12 10:41:33 +0200

    API configuration and authentication

--- config/services.php
+++ config/services.php
@@ -31,4 +31,15 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'linkedin' => [
+        'client_id' => env('LINKEDIN_CLIENT_ID'),
+        'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
+        'redirect' => env('LINKEDIN_REDIRECT_URI'),
+    ],
+
+    'infojobs' => [
+        'client_id' => env('INFOJOBS_CLIENT_ID'),
+        'client_secret' => env('INFOJOBS_CLIENT_SECRET'),
+        'redirect' => env('INFOJOBS_REDIRECT_URI'),
+    ],
 ];
